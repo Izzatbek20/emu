d2052a32bc5efd502d609e1d0c0ae1352cb9adf3
Izzatbek20 <izzatbekmajidov200020@gmail.com>
2024-05-12 16:47:57 +0500

    youtub player

--- src/components/FikirlarItem.vue
+++ src/components/FikirlarItem.vue
@@ -3,10 +3,13 @@
         <div class="flex flex-col justify-between gap-5">
             <div class="flex-1">
                 <div class="relative">
-                    <img :src="url" alt="image" class="rounded-lg rounded-bl-[3rem] object-cover object-center"
-                        srcset="">
-                    <Play class="size-20 absolute cursor-pointer bottom-2 left-2 max-lg:bottom-3 max-lg:left-3 max-lg:size-14"
-                        @click="openModal" />
+                    <img v-if="!video" :src="url" alt="image"
+                        class="rounded-lg rounded-bl-[3rem] object-cover object-center" srcset="">
+                    <div :id="id" class="rounded-lg rounded-bl-[3rem] object-cover object-center w-full">
+                    </div>
+                    <Play v-if="!video"
+                        class="size-20 absolute cursor-pointer bottom-2 left-2 max-lg:bottom-3 max-lg:left-3 max-lg:size-14"
+                        @click="playVideo(v)" />
                 </div>
             </div>
             <div class="flex-1">
@@ -21,7 +24,7 @@
             </div>
         </div>
 
-        <YoutubeModal :isOpen="isOpen" :v="v" @close="closeModal" />
+        <!-- <YoutubeModal :isOpen="isOpen" :v="v" @close="closeModal" /> -->
     </Card>
 </template>
 <script>
@@ -36,14 +39,51 @@ export default {
     },
     data() {
         return {
+            id: "youtube_" + (new Date().getTime()),
             url: null,
-            isOpen: false
+            isOpen: false,
+            video: false,
+            player: null
         }
+    },
+    mounted() {
+        document.addEventListener('click', this.handleClickOutside);
     },
     beforeMount() {
         this.url = `https://img.youtube.com/vi/${this.v}/mqdefault.jpg`
     },
     methods: {
+        playVideo() {
+            this.video = true
+            this.initYoutubeVideo()
+        },
+        initYoutubeVideo() {
+            const pl = this.player = new YT.Player(this.id, {
+                height: '100%',
+                width: '100%',
+                videoId: this.v,
+                playerVars: {
+                    'playsinline': 1
+                },
+                events: {
+                    'onReady': this.onPlayerReady,
+                    'onStateChange': this.onPlayerStateChange
+                }
+            });
+        },
+        onPlayerReady(event) {
+            event.target.playVideo();
+        },
+        onPlayerStateChange(event) {
+            if (event.data === 0 && this.video) {
+                this.stopVideo();
+                this.video = false;
+            }
+        },
+        stopVideo() {
+            this.player.stopVideo();
+            this.player.destroy();
+        },
         closeModal() {
             this.isOpen = false
         },
