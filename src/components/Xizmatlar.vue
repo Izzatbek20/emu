fe0ce89e03536d87fbc927b0d61bc23e7a2ba243
Izzatbek20 <izzatbekmajidov200020@gmail.com>
2024-06-14 11:01:32 +0500

    Xizmatlar card tuzatildi

--- src/components/Xizmatlar.vue
+++ src/components/Xizmatlar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid grid-cols-3 max-xl:grid-cols-2 max-sm:grid-cols-1 gap-7 mt-10">
 
-        <div v-for="(item, i) in items" :key="i"
+        <div v-for="(item, i) in data" :key="i"
             class="p-5 bg-violet flex items-center gap-4 rounded-2xl h-48 max-sm:h-40 max-lg:h-40 relative overflow-hidden cursor-pointer"
             @click="$router.push({ name: item.link })">
             <div class="h5 max-xl:h5 max-sm:h7 max-lg:h6 max-lg:font-extralight text-white w-1/2 max-xl:w-2/3 z-20">{{
@@ -12,12 +12,78 @@
     </div>
 </template>
 <script>
+import kargo from '@/assets/images/kargo.png'
+import { mapState } from 'vuex';
+
 export default {
-    props: {
-        items: {
-            type: Array,
-            required: true
+    data() {
+        return {
+            data: [],
+            origin: import.meta.env.VITE_EMU_API_ORIGIN,
         }
+    },
+    methods: {
+        async fetchData(newVal, locale) {
+            if (newVal) {
+                const data = [];
+                newVal.forEach(element => {
+                    const item = element.langs.find(langItem => langItem.lang === locale);
+                    if (item) {
+                        data.push({
+                            image: `${this.origin}/${element.image.replaceAll(/\\/g, '/')}`,
+                            title: item.title,
+                            link: this.linkDetect(element.id)
+                        });
+                    }
+                });
+                data.push({
+                    title: this.$t('sub.kargo'),
+                    image: kargo,
+                    link: "kargo"
+                });
+                this.data = data;
+            }
+        },
+        linkDetect(id) {
+
+            switch (id) {
+                case 1:
+                    return "kuryerXizmati"
+                    break;
+                case 2:
+                    return "xalqaroKuryerlikXizmati"
+                    break;
+                case 3:
+                    return "ustamaTolovli"
+                    break;
+                case 4:
+                    return "fullfilmentView"
+                    break;
+                case 5:
+                    return "individual"
+                    break;
+
+                default:
+                    break;
+            }
+            return
+        },
+    },
+    watch: {
+        serviceAll(newVal) {
+            this.fetchData(newVal, this.$i18n.locale);
+        },
+        '$i18n.locale'(newVal) {
+            this.fetchData(this.serviceAll, newVal);
+        },
+    },
+    computed: {
+        ...mapState({
+            serviceAll: state => state.emuAdmin.serviceAll,
+        })
+    },
+    mounted() {
+        this.$store.dispatch('serviceAll')
     }
 }
 </script>
